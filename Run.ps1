6a573a4ce8269a90f841958c9c21b22c2e731a61
Martin Szabo <martin.szabo333@gmail.com>
2024-12-13 15:30:59 +0100

    Adding EnabledScripts and Task

--- Run.ps1
+++ Run.ps1
@@ -1,5 +1,7 @@
 while ($True)
 {
-    Get-ChildItem -Path "$PSScriptRoot\Scripts" -Exclude $MyInvocation.MyCommand.Name | Select-Object -Expand FullName | ForEach-Object { . $_ }
+    [String[]] $IncludeScripts = Get-Content "$PSScriptRoot\.EnabledScripts"
+    $ExecuteScripts = Get-ChildItem -Path "$PSScriptRoot\Scripts" | Where-Object { $_.BaseName -in $IncludeScripts }
+    $ExecuteScripts.FullName | ForEach-Object { . $_ }
     Start-Sleep -Seconds 10
 }
