c2173f60955af968a83abcd8d59b043980da99ae
Martin Szabo <martin.szabo333@gmail.com>
2024-12-13 19:09:42 +0100

    Rename Run.cmd to RUN.cmd

---
+++ RUN.cmd
@@ -0,0 +1,2 @@
+cd /d %~dp0
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ep Bypass .\Bin\Run.ps1
--- Run.cmd
+++
@@ -1,2 +0,0 @@
-cd /d %~dp0
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ep Bypass .\Bin\Run.ps1
